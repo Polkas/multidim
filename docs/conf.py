9b8691d1a30619ac82083ef50bf03883c030fabd
Maciej Nasinski <nasinski.maciej@gmail.com>
2023-10-04 10:02:44 +0200

    test

--- docs/conf.py
+++ docs/conf.py
@@ -6,22 +6,23 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'multidim'
-copyright = '2023, MN'
-author = 'MN'
+project = "multidim"
+copyright = "2023, MN"
+author = "MN"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
-
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
-
-
+extensions = [
+    "myst_nb",
+    "autoapi.extension",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.viewcode",
+]
+autoapi_dirs = ["../src"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+html_static_path = ["lectures"]
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_static_path = ["_static"]
